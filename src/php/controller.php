1d621aa91d36e13e7ba5f3ec5f4de161d7728e2a
AnHaNguyen <edge.skywalker@gmail.com>
2016-10-21 20:30:13 +0800

    add duplicate check

--- src/php/controller.php
+++ src/php/controller.php
@@ -2,13 +2,13 @@
 	require_once('repo.php');
 	require_once('contributors.php');
 	require_once('history.php');
-	require_once('connects3.php');
+	//require_once('connects3.php');
 	ini_set('max_execution_time', 300);	//give 5 mins to clone a repo
 	session_start();
 	
-	execute($command='addrepo',$repo="https://github.com/AnHaNguyen/SPA");
-	execute($command='getcontributors',null,null,null,null,null,'','');
-	execute($command='getcommithistory', null,null, $user="AnHaNguyen",null,null,'','');
+	// execute($command='addrepo',$repo="https://github.com/AnHaNguyen/SPA");
+	// execute($command='getcontributors',null,null,null,null,null,'','');
+	// execute($command='getcommithistory', null,null, $user="AnHaNguyen",null,null,'','');
 
 	function execute($command, $repo=null, $email=null,$user=null, $start=null,$file=null,$s='', $e ='') {
 		switch (strtolower($command)) {
@@ -19,21 +19,21 @@
 				$repoObj = new Repo($repo);
 
 				$repoObj->initialize();
-				sendToCloud($repo, "repos");
-				echo("success");
+				//sendToCloud($repo, "repos");
+				//echo("success");
 				return("success");
 				break;
 			case 'addemail':
 				if ($email === null) {
 					exit("Please include an email address");
 				}
-				sendToCloud($email, "emails");
-				echo("success");
+				//sendToCloud($email, "emails");
+				//echo("success");
 				return("success");
 				break;
 			case 'getcontributors':
 				$list = getAllUsers();
-				echo(json_encode($list));
+				//echo(json_encode($list));
 				return($list);
 				break;
 			case 'getcommithistory':
@@ -41,7 +41,7 @@
 					exit("Please include a username");
 				}
 				$list = getHistoryUser($user, $start);
-				echo(json_encode($list));
+				//echo(json_encode($list));
 				return($list);
 				break;
 			case 'getfilehistory':
@@ -49,7 +49,7 @@
 					exit("Please include a file to view history");
 				}
 				$list = getHistoryFile($file, [$s,$e]);
-				echo(json_encode($list));
+				//echo(json_encode($list));
 				return($list);
 				break;
 			default:
