32834a449ccb9493b4964e0c5c04e7a0bf18a1c2
Jia Min <jiaminw@ymail.com>
2016-10-27 12:38:33 +0800

    Updated view for 1b

--- src/assets/js/app02.js
+++ src/assets/js/app02.js
@@ -1,9 +1,24 @@
+function createCheckboxes(data){
+    d3.select("div#checkboxArea").selectAll("input")
+    .data(data)
+    .enter()
+    .append('label')
+    .attr('for',function(d,i){ return d.name; })
+    .text(function(d) { return d; })
+    .append("input")
+    .attr("checked", true)
+    .attr("type", "checkbox")
+    .attr("id", function(d,i) { return d.name; })
+    .attr("onClick", "change(this)")
+    .append('br');
+}
+
 function drawLineGraph(data){
     
     // Set the dimensions of the canvas / graph
     var margin = {top: 30, right: 20, bottom: 30, left: 50},
-    width = 600 - margin.left - margin.right,
-    height = 270 - margin.top - margin.bottom;
+    width = 960 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
     
     // Parse the date / time
     var parseDate = d3.time.format("%Y-%m-%d").parse;
@@ -57,7 +72,7 @@ function drawLineGraph(data){
     svg.selectAll("dot")
     .data(data)
     .enter().append("circle")
-    .attr("r", 5)
+    .attr("r", 3)
     .attr("cx", function(d) { return x(d.date); })
     .attr("cy", function(d) { return y(d.totalNum); })
     .on("mouseover", function(d) {
@@ -85,4 +100,30 @@ function drawLineGraph(data){
     .attr("class", "y axis")
     .call(yAxis);
 
+}
+
+function drawTable(tableData, githubLink){
+    for (var i = 0; i < tableData.length; i++) {
+        var counter = tableData[i];
+        var row = document.createElement("tr");
+        var col1 = document.createElement("td"); //date
+        var col2 = document.createElement("td"); //hash
+        
+        var date = document.createTextNode(counter.date);
+        var a = document.createElement("a");
+        var hash = document.createTextNode(counter.hash);
+        a.appendChild(hash);
+        var link = githubLink.substring( 1, githubLink.indexOf(".git"));
+        link = link.replace(/^"(.*)"$/,'$1');
+        a.href = link+"/tree/"+counter.hash;
+        a.setAttribute('target','_blank');
+        
+        col1.appendChild(date);
+        col2.appendChild(a);
+        
+        row.appendChild(col1);
+        row.appendChild(col2);
+        
+        document.getElementById("tablebody").appendChild(row);
+    }
 }
