5be458507fb8fb9653a97bffebb0eabd397d27b5
bihuutue <bihuutue@gmail.com>
2016-10-29 04:53:07 +0800

    Change timezone to SGT, revamp content, read last log in

--- src/Lambda function/LambaSendMail.py
+++ src/Lambda function/LambaSendMail.py
@@ -1,5 +1,6 @@
 import smtplib
 import datetime
+from datetime import timedelta
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 import json
@@ -19,62 +20,93 @@ def lambda_handler(event, context):
     
     #setup the bucket
     s3 = boto3.resource('s3')
-    bucket = s3.Bucket('testnoti')
+    bucket = s3.Bucket('3219')
     exists = True
     try:
-        s3.meta.client.head_bucket(Bucket='testnoti')
+        s3.meta.client.head_bucket(Bucket='3219')
     except botocore.exceptions.ClientError as e:
         # If a client error is thrown, then check that it was a 404 error.
         # If it was a 404 error, then the bucket does not exist.
         error_code = int(e.response['Error']['Code'])
         if error_code == 404:
             exists = False
+            return "Bucket does not exist"
     
     
     server.login(sender+"@gmail.com", password)
-    #subList = ["bihuutue@gmail.com", "edge.skywalker@gmail.com"]
-    subList = ["bihuutue@gmail.com"]
     
-    #Read JSON data from S3
-    S3Obj = s3.Object('testnoti', 'data.json')
-    data = json.load(S3Obj.get()["Body"])
+    #Read updated data from S3
+    try:
+        S3Obj = s3.Object('3219', 'emails.json')
+        updatedData = json.load(S3Obj.get()["Body"])
+    except botocore.exceptions.ClientError as e:
+        error_code = e.response['Error']['Code']
+        if error_code == "404":
+            exists = False
+            return "File emails.json does not exist"
+    
+    #Read existing data from S3
+    try:
+        S3Obj = s3.Object('3219', 'data.json')
+        existingData = json.load(S3Obj.get()["Body"])
+    except botocore.exceptions.ClientError as e:
+        error_code = e.response['Error']['Code']
+        if error_code == "404" or "NoSuchKey":
+            exists = False
+            existingData = []
+
+    subList = []
+    lastSentList = []
+    for mailRecord in existingData:
+        subList.append(mailRecord['address'])
+        lastSentList.append(mailRecord['lastSent'])
+    #subList = ["bihuutue@gmail.com"]
     
     
-    time = datetime.datetime.now().strftime("%d-%b-%Y %H:%M")
-    msg = MIMEMultipart()
-    msg['From'] = sender
-    msg['To'] = "Subscribers"
-    msg['Subject'] = "TEST NOTIFICATION"
-    bodyPart1 = "A new notification sent at: " + str(time) + "! \n"
-    
-    for sub in subList:
-    	body = bodyPart1
-    	
-    	sinceLastSent = "This is the first email!"
-    	for mailRecord in data:
-    		if mailRecord['address'] == sub:
-    			lastSent = datetime.datetime.strptime(mailRecord['lastSent'], "%d-%b-%Y %H:%M")
-    			delta = datetime.datetime.now() - lastSent
-    			sinceLastSent = "It has been "+ str(delta.days)+ " days since you last received a notification!"
-    			break
-    	body += sinceLastSent
-    	msg.attach(MIMEText(body, 'plain'))
-    	server.sendmail(sender, sub, msg.as_string() + sinceLastSent)
-    server.quit()
-    
-    print("Email sent!")
-    
+   
     data = []
-    for sub in subList:
-    	mailRecord = {
-    		'address' : sub,
-    		'lastSent' : time
-    	}
-    	data.append(mailRecord)
-    	
-    print(data)
+
+    for mailRecord in updatedData:
+        time = datetime.datetime.now() + timedelta(hours=8)
+        msg = MIMEMultipart()
+        msg['From'] = sender
+        msg['To'] = "Subscribers"
+        msg['Subject'] = "TEST NOTIFICATION"
+        address = mailRecord['address']
+        sinceLastLogin = ""
+        sinceLastSent = "This is the first email! \n"
+        username = address.split("@")[0]
+        header = "Dear " + username + ", \n \n"
+        notification = "This notification was sent at: " + str(time.strftime("%d-%b-%Y %H:%M")) + "SGT! \n"
+        signature = "Best Regards, \n"+"THJJ Team"
+
+        
+        if address in subList:
+            lastLogin = datetime.datetime.strptime(mailRecord['lastSent'], "%d-%b-%Y %H:%M")
+            print(time, lastLogin)
+            delta = time - lastLogin
+            sinceLastLogin = "It has been "+ str(delta.days)+ " days since you last logged in! \n"
+            
+            lastSent = datetime.datetime.strptime(lastSentList[subList.index(address)], "%d-%b-%Y %H:%M")
+            delta = time - lastSent
+            sinceLastSent = "It has been "+ str(delta.days)+ " days since the last notification! \n \n"
+
+        
+        body = header + notification + sinceLastLogin + sinceLastSent + signature
+        msg.attach(MIMEText(body, 'plain'))
+        server.sendmail(sender, address, msg.as_string())
+        print(username + msg.as_string())
+
+        newRecord =  {
+            'address' : address,
+            'lastSent' : time.strftime("%d-%b-%Y %H:%M")
+        }
+        data.append(newRecord)
     
+    
+    server.quit()
+    print("Email sent!")
     
     #Write JSON data to S3
     S3Obj.put(Body= json.dumps(data))
-    return str(lastSent)
+    return "Email sent!"
