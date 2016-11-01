96934be5b1dc4477413f9316ef9c08869aabd0e9
AnHaNguyen <edge.skywalker@gmail.com>
2016-11-01 19:09:54 +0800

    add 1e and fix bugs 1d

--- src/php/contributors.php
+++ src/php/contributors.php
@@ -1,7 +1,11 @@
 <?php
 	require_once("user.php");
 	require_once("util.php");
-
+	require_once("history.php");
+	// chdir('../../repos/SPA/');
+	// $list = getLines();
+	// echo(json_encode($list));
+	
 	function getAllUsers() {
 		//redirect();
 		$out = array();
@@ -34,5 +38,37 @@
 			$totalDel += intval($tokens[1]);
 		}
 		return array($totalIns, $totalDel);
+	}
+
+	function getLines() {
+		$out = array();
+		$command1 = 'git ls-files';
+		exec($command1, $out);
+		$list = array();
+		foreach($out as $file) {
+			$history = getHistoryFile(trim_all($file));
+			for ($i = 0; $i < sizeof($history); $i++) {
+				$e = $history[$i];	
+				$isFound = false;
+				for ($j = 0; $j < sizeof($list); $j++) {
+					if (strcmp($list[$j]->name,$e->getAuthor()) === 0) {
+						$list[$j]->lineNum += sizeof($e->getLines());
+						$isFound = true;
+						break;
+					}
+				}
+				if (!$isFound) {
+					$author = new stdClass();
+					$author->name = $e->getAuthor();
+					if ($author->name === false) {
+						echo(trim_all($file));
+					}
+					$author->lineNum = sizeof($e->getLines());
+					array_push($list, $author);
+				}				
+			}
+		}
+
+		return $list;
 	}
 ?>
